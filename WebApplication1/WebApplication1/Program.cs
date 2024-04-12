1cebfbe2349927a4000e6c34ee999bbf084726d9
Piotr Suwara <s28023@pjwstk.edu.pl>
2024-04-12 14:21:44 +0200

    Finito

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -27,6 +27,15 @@ var _zwierzeta = new List<Zwierze>
     new Zwierze { Id = 4, Imie = "Kajtek", Kategoria = "Pies", Masa = 8, KolorSiersci = "Brązowy" }
 };
 
+var _wizyty = new List<Wizyta>
+{
+    new Wizyta { Id = 1, Data = "2021-10-10", IdZwierzecia = 1, Opis = "Badanie ogólne", cena = 50 },
+    new Wizyta { Id = 2, Data = "2021-10-11", IdZwierzecia = 2, Opis = "Szczepienie", cena = 30 },
+    new Wizyta { Id = 3, Data = "2021-10-12", IdZwierzecia = 3, Opis = "Badanie ogólne", cena = 50 },
+    new Wizyta { Id = 4, Data = "2021-10-13", IdZwierzecia = 4, Opis = "Szczepienie", cena = 30 },
+    new Wizyta { Id = 5, Data = "2021-10-14", IdZwierzecia = 1, Opis = "Badanie ogólne", cena = 50 },
+};
+
 app.MapGet("/api/Zwierzeta",() => Results.Ok(_zwierzeta)).WithName("GetZwierzeta").WithOpenApi();
 app.MapGet("/api/Zwierzeta/{id:int}", (int id) =>
 {
@@ -65,6 +74,22 @@ app.MapDelete("api/Zwierzeta/{id:int}", (int id) =>
     _zwierzeta.Remove(zwierze);
     return Results.Ok();
 }).WithName("DeleteZwierze").WithOpenApi();
-
+app.MapGet("api/Wizyty/{id:int}", (int id) =>
+{
+    var wizyty = new List<Wizyta>{};
+    for(int i = 0; i < _wizyty.Count; i++)
+    {
+        if(_wizyty[i].IdZwierzecia == id)
+        {
+            wizyty.Add(_wizyty[i]);
+        }
+    }
+    return Results.Ok(wizyty);
+}).WithName("GetWizyta").WithOpenApi();
+app.MapPost("api/Wizyty", (Wizyta wizyta) =>
+{
+    _wizyty.Add(wizyta);
+    return Results.StatusCode(StatusCodes.Status201Created);
+}).WithName("PostWizyta").WithOpenApi();
 
 app.Run();
